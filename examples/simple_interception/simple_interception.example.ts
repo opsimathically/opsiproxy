2f4a04dcf7950b47eb5c13a396e9571c93d10978
iratefun <jason.a.medeiros@proton.me>
2025-04-03 08:48:08 -1000

    Updating gitignore.

--- examples/simple_interception/simple_interception.example.ts
+++ examples/simple_interception/simple_interception.example.ts
@@ -1,6 +1,11 @@
 /* eslint-disable no-debugger */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%% Intercept HTTP/HTTPs Requests Using Plugin %%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 import test from 'node:test';
 import assert from 'node:assert';
 import { deepEqual } from 'fast-equals';
