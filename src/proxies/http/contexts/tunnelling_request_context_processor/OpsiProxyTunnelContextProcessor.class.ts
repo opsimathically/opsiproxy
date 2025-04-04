a093887190bf8dd0ffb30a2c9c37678fe93f2804
iratefun <jason.a.medeiros@proton.me>
2025-04-04 08:30:54 -1000

    Renaming .js to .ts.  Old habits die hard.

--- src/proxies/http/contexts/tunnelling_request_context_processor/OpsiProxyTunnelContextProcessor.class.ts
+++ src/proxies/http/contexts/tunnelling_request_context_processor/OpsiProxyTunnelContextProcessor.class.ts
@@ -7,8 +7,18 @@ HTTP Proxies use either forward requests, or tunnels.  This class
 handles the context in which a tunnel is made.
 */
 
+import { OpsiHTTPProxy } from '@src/proxies/http/OpsiHTTPProxy.class.js';
+
+type proxy_tunnel_context_options_t = {
+  proxy: OpsiHTTPProxy;
+};
+
 class OpsiProxyTunnelContextProcessor {
-  constructor() {}
+  options: proxy_tunnel_context_options_t;
+
+  constructor(options: proxy_tunnel_context_options_t) {
+    this.options = options;
+  }
 }
 
 export { OpsiProxyTunnelContextProcessor };
